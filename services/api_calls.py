d08137e5e5aaeabe1a032e819e753736bff435e0
gunitavibe <iamgunita705@gmail.com>
2026-04-06 14:49:46 +0530

    added error handling

--- services/api_calls.py
+++ services/api_calls.py
@@ -1,21 +1,33 @@
 import httpx
 
 async def get_joke():
-    async with httpx.AsyncClient() as client:
-        res = await client.get("https://official-joke-api.appspot.com/random_joke")
-        data = res.json()
-        return f"{data['setup']} - {data['punchline']}"
+    try:
+        async with httpx.AsyncClient() as client:
+            res = await client.get("https://official-joke-api.appspot.com/random_joke")
+            data = res.json()
+            return f"{data['setup']} - {data['punchline']}"
+    except:
+        return "Error fetching joke."
+
 
 async def get_user():
-    async with httpx.AsyncClient() as client :
-        res = await client.get("https://randomuser.me/api/") 
-        data = res.json()
-        user = data["results"][0]
-        return f"{user['user']['first']} {user['name']['last']}"
+    try:
+        async with httpx.AsyncClient() as client:
+            res = await client.get("https://randomuser.me/api/")
+            data = res.json()
+
+            user = data["results"][0]
+            return f"{user['name']['first']} {user['name']['last']}"
+
+    except Exception as e:
+        return "Error fetching user. Try again."
     
 async def get_advice():
-    async with httpx.AsyncClient() as client :
-        res = await client.get("https://api.adviceslip.com/advice")
-        data = res.json()
-        return data["slip"]["advice"]    
+    try:
+        async with httpx.AsyncClient() as client:
+            res = await client.get("https://api.adviceslip.com/advice")
+            data = res.json()
+            return data["slip"]["advice"]
+    except:
+        return "Error fetching advice."  
 
